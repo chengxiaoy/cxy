b314ab6d32f4b3a7fd4f128b6b05c1d87d6c0e47
e <chengyingjuly@163.com>
2019-06-15 17:02:20 +0800

    submit py

--- submit.py
+++ submit.py
@@ -17,7 +17,7 @@ from tqdm import tqdm
 submission = pd.read_csv('Faces_in_the_Wild/sample_submission.csv')
 
 predictions = []
-model = SiameseNetwork(False).to(device).eval()
+model = SiameseNetwork(False,True).to(device).eval()
 
 for batch in tqdm(chunker(submission.img_pair.values)):
     X1 = [x.split("-")[0] for x in batch]
