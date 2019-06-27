3d4799241d6bd67c6675e06dfaef98677d12adb6
cheng ying <chengying@tezign.com>
2019-06-27 13:21:14 +0800

    shuffle = true

--- tets.py
+++ tets.py
@@ -170,7 +170,7 @@ def train_model(model, criterion, optimizer, scheduler, dataloaders, num_epochs=
     best_model_wts = copy.deepcopy(model.state_dict())
     min_loss = float('inf')
     max_acc = 0.0
-    epoch_nums = {'train': 200, 'val': 70}
+    epoch_nums = {'train': 700, 'val': 70}
     for epoch in range(num_epochs):
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
         print('-' * 10)
