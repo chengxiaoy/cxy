bfbc17a0f7237131dd942afdd6edb8bb7f66c21c
cheng ying <chengying@tezign.com>
2019-06-28 20:50:57 +0800

    shuffle=None

--- submit.py
+++ submit.py
@@ -22,7 +22,7 @@ model = SiameseNetwork(False).to(device).eval()
 
 model.load_state_dict(torch.load('SiameseNetwork.pth'))
 
-model.eval()
+model.train()
 
 for batch in tqdm(chunker(submission.img_pair.values)):
     X1 = [x.split("-")[0] for x in batch]
