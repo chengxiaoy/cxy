1d08980bf0ba0c85510109f0bd2756cb4efbff65
cheng ying <chengying@tezign.com>
2019-06-28 14:40:12 +0800

    shuffle=None

--- tets.py
+++ tets.py
@@ -27,7 +27,7 @@ import math
 
 writer = SummaryWriter(logdir=os.path.join("../tb_log", datetime.now().strftime('%b%d_%H-%M-%S')))
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
 
 
 class Config():
