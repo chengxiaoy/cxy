84d6b50d3d17f3ee4556fd8211379afa39a5a084
cheng ying <chengying@tezign.com>
2019-07-02 10:25:29 +0800

    shuffle=None

--- tets.py
+++ tets.py
@@ -68,7 +68,7 @@ class SiameseNetwork(nn.Module):
         self.globalavg = nn.AdaptiveAvgPool2d(1)
 
         self.dropout2 = nn.Dropout(0.3)
-        self.bn1 = nn.BatchNorm2d(2048)
+        self.bn1 = nn.BatchNorm2d(512)
         self.bn2 = nn.BatchNorm1d(512)
 
     def forward_once(self, x):
