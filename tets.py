13a647319f11f6e546451ab4e8fbe2713a081819
cheng ying <chengying@tezign.com>
2019-07-09 16:19:38 +0800

    commit

--- tets.py
+++ tets.py
@@ -68,8 +68,8 @@ class SiameseNetwork(nn.Module):
         self.dropout = nn.Dropout(0.01)
         self.ll2 = nn.Linear(100, 1)
 
-        self.bilinear = nn.Bilinear(512, 512, 1024)
-        self.lll = nn.Linear(1024, 100)
+        self.bilinear = nn.Bilinear(512, 512, 2048)
+        self.lll = nn.Linear(2048, 100)
         self.ll = nn.Linear(2048, 512)
 
         self.conv = nn.Conv2d(2048, 512, 1)
